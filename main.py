a549f6ae03450f469ee3f9de13aa603512a24d42
madresz04 <madresz04@gmail.com>
2025-07-30 21:56:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, filters
 
 # --- Állítsd be a TOKEN-t ---
-BOT_TOKEN = "7764705724:AAE_5U4kt5_iCe0B-m9ZSRgYhMpS76mpgg"
+TOKEN = "7764705724:AAE_5U4kt5_iCe0B-m9ZSRgYhMpS76mpgg"
 
 # --- Admin értesítéshez Telegram ID ---
 ADMIN_ID = "christtfxg"  # <- ezt írd át a saját vagy az ügyfél Telegram user ID-jére
